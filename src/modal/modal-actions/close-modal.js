effc93541931e4c055a20c0e0f95451bc523258b
David Miranda <david@storylog.com>
2020-06-21 20:12:58 -0400

    Update close-modal.js

--- src/modal/modal-actions/close-modal.js
+++ src/modal/modal-actions/close-modal.js
@@ -1,29 +1,28 @@
 import { enableBodyScroll } from 'body-scroll-lock';
-import globals from "../helpers/globals";
+import temporaryGlobals from "../../temporary-globals/temporary-globals";
 
 export default function closeModal() {
-  document.body.classList.remove(globals.BODY_CLASS_MODAL_OPEN);
+  document.body.classList.remove(temporaryGlobals.BODY_CLASS_MODAL_OPEN);
 
   // TODO: keep track of when the modal was closed
   // setItemLocalStorage("modalClosedDate", Date.now());
 
   // hide the modal
-  globals.modalElem.style.display = 'none';
+  temporaryGlobals.MODAL_ELEM.style.display = 'none';
 
   // Untrap the tab focus by removing tabindex=-1. You should restore previous values if an element had them.    
-  const focusableElements = document.querySelectorAll(globals.FOCUSABLE_SELECTORS);
+  const focusableElements = document.querySelectorAll(temporaryGlobals.FOCUSABLE_SELECTORS);
   focusableElements.forEach(el => el.removeAttribute('tabindex'));
   
   // Untrap screen reader focus
   const topLevelElements = document.querySelectorAll("body > [aria-hidden]");
   topLevelElements.forEach(el => el.removeAttribute('aria-hidden'));
-  globals.modalElem.setAttribute('aria-hidden', 'true');
+  temporaryGlobals.MODAL_ELEM.setAttribute('aria-hidden', 'true');
   
   // restore focus to the triggering element
-  let activeElement = globals.getActiveElement();
-  if (activeElement) {
-    activeElement.focus();
+  if (temporaryGlobals.activeElement) {
+    temporaryGlobals.activeElement.focus();
   }
 
-  enableBodyScroll(globals.modalElem);
+  enableBodyScroll(temporaryGlobals.MODAL_ELEM);
 }
