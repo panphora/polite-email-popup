6398303f23cc088019700135909b8a4d4b935349
David Miranda <david@storylog.com>
2020-06-22 12:08:45 -0400

    nicer styles for debug notice, fixed bug with debug notice, better layout for debug notice

--- src/debug-notice/get-debug-notice-template-string.js
+++ src/debug-notice/get-debug-notice-template-string.js
@@ -1,40 +1,46 @@
+import temporaryGlobals from "../temporary-globals/temporary-globals";
+
 export default function getDebugNoticeTemplateString ({requirements, actual, meetsRequirements}) {
   return `<div class="debug-emails-forms">
     <div>
-      <b>Page views: </b>
-      <span>${actual.pageViews} </span>
-      <span>(requires: ${requirements.pageViews}) </span>
+      <b class="label">Website type: </b>
+      <b>${temporaryGlobals.websiteType.charAt(0).toUpperCase() + temporaryGlobals.websiteType.slice(1)} website</b>
     </div>
     <div>
-      <b>Highest scroll percentage: </b>
-      <span>${actual.highestScrollPercentageAcrossAllPages}% </span>
-      <span>(requires: ${requirements.highestScrollPercentageAcrossAllPages}%) </span>
+      <b class="label">Page views: </b>
+      <b>${actual.pageViews} </b>
+      <span class="light-text">(requires: ${requirements.pageViews}) </span>
     </div>
     <div>
-      <b>Active time on site: </b>
-      <span>${actual.activeTimeOnSite}s </span>
-      <span>(requires: ${requirements.activeTimeOnSite}s) </span>
+      <b class="label">Highest scroll percentage: </b>
+      <b>${actual.highestScrollPercentageAcrossAllPages}% </b>
+      <span class="light-text">(requires: ${requirements.highestScrollPercentageAcrossAllPages}%) </span>
     </div>
     <div>
-      <b>Active time on page: </b>
-      <span>${actual.activeTimeOnPage}s </span>
-      <span>(${requirements.activeTimeOnPage}s) </span>
+      <b class="label">Active time on site: </b>
+      <b>${actual.activeTimeOnSite}s </b>
+      <span class="light-text">(requires: ${requirements.activeTimeOnSite}s) </span>
     </div>
     <div>
-      <b>Saw modal: </b>
+      <b class="label">Active time on page: </b>
+      <b>${actual.activeTimeOnPage}s </b>
+      <span class="light-text">(${requirements.activeTimeOnPage}s) </span>
+    </div>
+    <div>
+      <b class="label">Saw modal: </b>
       <span>never</span>
     </div>
     <div>
-      <b>Closed modal: </b>
+      <b class="label">Closed modal: </b>
       <span>never</span>
     </div>
     <div>
-      <b>Closed bottom bar: </b>
+      <b class="label">Closed bottom bar: </b>
       <span>never</span>
     </div>
     <div>
-      <b>Modal is openable: </b>
-      <span>${meetsRequirements}</span>
+      <b class="label">Modal is openable: </b>
+      ${meetsRequirements ? '<b class="green">Yes</b>' : '<b class="red">No</b>'}
     </div>
     <button class="debug-emails-forms__reset" type="button">Reset</button>
   </div>`;
--- src/debug-notice/init-debug-notice-event-listeners.js
+++ src/debug-notice/init-debug-notice-event-listeners.js
@@ -3,7 +3,7 @@ import temporaryGlobals from "../temporary-globals/temporary-globals";
 export default function initDebugNoticeEventListeners () {
   if (temporaryGlobals.showDebugNotice) {
     document.addEventListener("click", event => {
-      if (event.closest(temporaryGlobals.SELECTORS.DEBUG_ELEM_RESET_SELECTOR)) {
+      if (event.target.closest(temporaryGlobals.SELECTORS.DEBUG_ELEM_RESET_SELECTOR)) {
         window.localStorage.clear(); 
         window.location.reload();
       }
