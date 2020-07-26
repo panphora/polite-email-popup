0d8305398c4bb23d5feeda8e910f70d868cb6a6c
David Miranda <david@storylog.com>
2020-07-26 16:48:58 -0400

    adding styles into library

--- demo/js/main.js
+++ demo/js/main.js
@@ -5,11 +5,15 @@ import PoliteEmailPopup from "../../dist/polite-email-popup.modern.js";
 
 PoliteEmailPopup.contentWebsite({
   showDebugNotice: true, // remove in production
+  beSlightlyMoreAggressive: false,
   imageSrc: "/demo/images/remake-logo-icon.svg",
   headingText: "Sign up for Remake's newsletter",
   descriptionText: "Follow Remake's progress as we change web development",
   successMessage: "Thank you signing up!",
+  mainButtonText: "Sign up",
+  closeButtonText: "Close",
   onSubmit: function ({event, email}) {
     console.log("FORM SUBMITTED", event, email);
   }
 });
+
--- src/dom/init-dom-elements.js
+++ src/dom/init-dom-elements.js
@@ -3,8 +3,11 @@ import addHtmlToPage from "./add-html-to-page";
 import getModalTemplateString from "../modal/get-modal-template-string";
 import getBottomBarTemplateString from "../bottom-bar/get-bottom-bar-template-string";
 import initElemGlobals from "./init-elem-globals";
+import getStylesString from "../helpers/get-styles-string";
 
 export default function initDomElements () {
+  addHtmlToPage(getStylesString());
+
   if (temporaryGlobals.showDebugNotice) {
     addHtmlToPage("<div class='polite-email-popup__debug-notice'></div>");
   }
