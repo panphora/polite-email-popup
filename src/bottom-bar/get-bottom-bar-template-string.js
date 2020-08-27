d4a1b6924cd2b7baebbec8f97321d6d7249458a0
David Miranda <david@storylog.com>
2020-08-26 23:38:38 -0400

    better responsive styles for bottom bar

--- src/bottom-bar/get-bottom-bar-template-string.js
+++ src/bottom-bar/get-bottom-bar-template-string.js
@@ -1,14 +1,14 @@
 export default function getBottomBarTemplateString ({headingText, mainButtonText, closeButtonText}) {
   return `<div class="bottom-bar" aria-hidden="true">
   <form class="bottom-bar__content" method="POST" action="/">
-    <label class="bottom-bar__label">
-      <span class="bottom-bar__label-text">${headingText}</span>
-      <input class="bottom-bar__input email-signup-form-input" type="email" name="EMAIL" value="" placeholder="Your email address" required>
+    <label class="bottom-bar__label-text" for="bottom-bar-email">${headingText}</label>
+    <div class="bottom-bar__bottom">
+      <input id="bottom-bar-email" class="bottom-bar__input email-signup-form-input" type="email" name="EMAIL" value="" placeholder="Your email address" required>
       <div style="position: absolute; left: -5000px;" aria-hidden="true">
         <input class="js-validate-not-robot" type="text" tabindex="-1" value="">
       </div>
-    </label>
-    <button class="bottom-bar__sign-up" type="submit">${mainButtonText}</button>
+      <button class="bottom-bar__sign-up" type="submit">${mainButtonText}</button>
+    </div>
   </form>
   <div class="bottom-bar__close-container">
     <button class="bottom-bar__close" type="button">
